f778c30257663ac659fd1933d574ece6c48062a7
RuslanOreshko <3na3drocher@gmail.com>
2026-05-14 00:22:40 +0300

    feat: add service for file system

---
+++ FileSystemEmulator.Application/Services/FileSystemService.cs
@@ -0,0 +1,27 @@
+using FileSystemEmulator.Application.Commands;
+using FileSystemEmulator.Application.Menagers;
+using FileSystemEmulator.Core.Models;
+using FileSystemEmulator.Core.Interfaces;
+
+
+namespace FileSystemEmulator.Application.Services;
+
+public class FileSystemService
+{
+    private readonly UndoRedoMenager _manager = new();
+
+    public FileSystemService(ICommand command)
+    {
+        _manager.ExecuteCommand(command);
+    }
+
+    public void Undo()
+    {
+        _manager.Undo();
+    }
+
+    public void Redo()
+    {
+        _manager.Redo();
+    }
+}
